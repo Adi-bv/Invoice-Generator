00a1292d655497729505b68226a9f29da389b36f
adi-bv <123456789+svaditi029@gmail.com>
2024-07-05 20:42:27 +0530

    updated link of backend

--- Invoice-generator-backend/index.js
+++ Invoice-generator-backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://invoice-generator-rho-neon.vercel.app",
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
