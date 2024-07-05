3fe9157877b091aad325a66dd4ed732dbe733e09
Aditi Gupta <157955007+Adi-bv@users.noreply.github.com>
2024-07-05 17:05:49 +0530

    Update index.js

--- Invoice-generator-backend/index.js
+++ Invoice-generator-backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: " http://localhost:5173",
+  origin: "https://invoice-generator-xi-orcin.vercel.app",
   credentials: true,
 }));
 app.use(bodyParser.json());
