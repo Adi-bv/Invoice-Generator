fd5abf9cfc696ba342a0986ec698b72633e9a2a6
adi-bv <123456789+svaditi029@gmail.com>
2024-07-06 12:15:08 +0530

    improved mongo url

--- Invoice-generator-backend/connect.js
+++ Invoice-generator-backend/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
   try {
-    const conn = await mongoose.connect(process.env.URL);
+    const conn = await mongoose.connect(process.env.Mongo_MyDataBase_URL);
     console.log("connected to mongo");
   } catch (error) {
     console.log(error);
