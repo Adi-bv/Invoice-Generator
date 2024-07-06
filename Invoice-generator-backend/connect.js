1ae35e3641c1586ad544319d012d3733c83197d6
Aditi Gupta <157955007+Adi-bv@users.noreply.github.com>
2024-07-06 13:22:21 +0530

    Update connect.js url

--- Invoice-generator-backend/connect.js
+++ Invoice-generator-backend/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async() => {
   try {
     console.log(process.env.Mongo_MyDataBase_URL);
-    const conn = await mongoose.connect("mongodb+srv://adi_mongo:Mongo123@mycluster.6fnc5t6.mongodb.net/?retryWrites=true&w=majority&appName=mycluster");
+    const conn = await mongoose.connect(process.env.URL);
     
     console.log("connected to mongo");
   } catch (error) {
